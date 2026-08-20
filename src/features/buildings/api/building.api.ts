d3b588eef07cdc1e9ced91266acdef50231678e8
eyesoreee <salihjohaire@gmail.com>
2026-08-20 16:16:32 +0800

    used modified supbase

--- src/features/buildings/api/building.api.ts
+++ src/features/buildings/api/building.api.ts
@@ -1,8 +1,8 @@
-import { supabase } from "@/lib/supabase";
+import { getSupabase } from "@/lib/supabase";
 import { Building } from "../types/building.entity";
 
 export async function getBuildings(): Promise<Building[]> {
-  const { data, error } = await supabase.from("Building").select("*");
+  const { data, error } = await getSupabase().from("Building").select("*");
 
   if (error) {
     console.log(`Fetching BUILDINGS failed: ${error.message}`);
@@ -12,7 +12,7 @@ export async function getBuildings(): Promise<Building[]> {
 }
 
 export async function getBuilding(id: string): Promise<Building | null> {
-  const { data, error } = await supabase
+  const { data, error } = await getSupabase()
     .from("Building")
     .select("*")
     .eq("id", id)
